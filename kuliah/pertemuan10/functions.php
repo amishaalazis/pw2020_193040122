0f8fa1f752381d7214a5615b453ad5f656767636
amishaalazis <60495662+amishaalazis@users.noreply.github.com>
2020-04-20 12:29:52 +0700

    koneksi dan tampil data

---
+++ kuliah/pertemuan10/functions.php
@@ -0,0 +1,17 @@
+<?php
+function koneksi()
+{
+  return mysqli_connect('localhost', 'root', '', 'pw_193040122');
+}
+
+function query($query)
+{
+  $conn = koneksi();
+  $result = mysqli_query($conn, $query);
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+
+  return $rows;
+}
